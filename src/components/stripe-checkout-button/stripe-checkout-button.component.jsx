d1c17cd321faa3c6b2eeef733c2c64f686e68e46
cm <cpgmontebon@gmail.com>
2021-01-06 01:32:55 +0800

    integrate stripe payment

---
+++ src/components/stripe-checkout-button/stripe-checkout-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+import './stripe-checkout-button.styles.scss';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = process.env.REACT_APP_STRIPE_PK; 
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+    return (
+        <StripeCheckout 
+            label="Pay Now"
+            name="Talibaba"
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
