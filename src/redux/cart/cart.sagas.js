78fd520188b7ad8fccb50a2dd77f9dec041e42b6
cm <cpgmontebon@gmail.com>
2021-02-07 21:08:18 +0800

    add cart saga; clear cart when user signs out

---
+++ src/redux/cart/cart.sagas.js
@@ -0,0 +1,18 @@
+import { all, takeEvery, takeLatest, put, call } from 'redux-saga/effects';
+
+import UserActionTypes from '../user/user.types'
+import { clearCart } from './cart.actions'
+
+export function* clearCartItems() {
+    yield put(clearCart());
+}
+
+export function* onSignOutUser() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartItems)
+}
+
+export function* cartSaga() {
+    yield all([
+        call(onSignOutUser)
+    ])
+}
--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,12 +1,16 @@
 import { all, call } from 'redux-saga/effects';
 
 import { userSaga } from './user/user.sagas'
+import { cartSaga } from './cart/cart.sagas'
+
 import { fetchCollectionStart } from './shop/shop.sagas';
+
 
 export default function* rootSaga() {
     yield all([
         call(fetchCollectionStart),
-        call(userSaga)
+        call(userSaga),
+        call(cartSaga),
     ])
 }
 
